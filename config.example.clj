4a2abefc7da7ce4e6d9c10cc202f56f38879e879
Ivan Stefanischin <ivan.stefanischin@bevuta.com>
2017-08-28 17:25:26 +0200

    adjust the comments

--- config.example.clj
+++ config.example.clj
@@ -1,22 +1,19 @@
 {;; The common section contains all options not directly constrained
- ;; to a testing section but may used for common purposed by the
+ ;; to a testing section but may used for common purposes by the
  ;; testing sections below.
- :common {;; Registration realm
+ :common {;; Registration realm.
           :realm "asterisk"
 
-          ;; Network address of the local SIP-client
+          ;; Network address of the local SIP-client.
           :local-address "192.168.178.10"
 
-          ;; Network address of the B2BUA the SIP-clients will be
-          ;; registered on
+          ;; Network address of the B2BUA containing the appropriate
+          ;; SIP-accounts for the SIP-clients configured in this file.
           :registrar-address "10.10.10.10"
-
-          ;; Buffer size of the event channels.
-          :event-channel-buffer-size 200
 
           ;; Registration timeout in seconds. If this time exceeds
           ;; after the last resubscription, the SIP-client will be
-          ;; treated as unavailable by the B2BUA
+          ;; treated as unavailable by the B2BUA.
           :register-ttl-s 120
 
           ;; Time in seconds the SIP-client will be resubscribed to
@@ -26,8 +23,12 @@
           ;; Time in milliseconds to wait for the excpected event by
           ;; the expect-events! function. Assuming a default T1 of
           ;; 500ms we use the default of 64 * T1 according to the
-          ;; RFC3261
-          :default-timeout-ms 32000}
+          ;; RFC3261.
+          :default-timeout-ms 32000
+
+          ;; Buffer size of the event channels.
+          :event-channel-buffer-size 200}
+
  ;; This testing section contains the options required by the unit
  ;; tests.
  :unit-tests {;; Three SIP-clients. There are exactly three because
@@ -38,8 +39,9 @@
                           ;; appears as a text representation of the
                           ;; SIP-address.
                           :display-name "Thomas Watson"
+
                           ;; The local port of the SIP-client. Usually
-                          ;; its 5060 or 5061 bus here we use multiple
+                          ;; its 5060 or 5061 but here we use multiple
                           ;; SIP-clients on the same machine, so we
                           ;; use the dynamic/private port
                           ;; range (49152-65535). This range can be
@@ -48,22 +50,26 @@
                           ;; here and aren't allocated dynamicly, so
                           ;; there is a chance that these ports are
                           ;; used by already running applications.
-                          :local-port 5061
+                          :local-port 49152
+
                           ;; The extension to the call number
                           ;; registered on the B2BUA for this
                           ;; SIP-account.
                           :extension 1001
+
                           ;; The authentication username.
                           :user "watson"
+
                           ;; The authentication password.
                           :password "thomaswatson"}
+
                          {:display-name "Graham Bell"
-                          :local-port 5062
+                          :local-port 49153
                           :extension 1002
                           :user "bell"
                           :password "grahambell"}
                          {:display-name "Elisha Gray"
-                          :local-port 5063
+                          :local-port 49154
                           :extension 1003
                           :user "gray"
                           :password "elishagray"}]}}
@@ -72,7 +78,7 @@
  ;; tests have a configurable amount of clients.
  :performance-test {;; A function which gets an index number and
                     ;; returns the configuration of a SIP-client
-                    ;; asigned to this index
+                    ;; assigned to this index.
                     :client-fn (fn [i]
                                  (let [id (+ 1000 i)]
                                    {:display-name (str "Testuser " id)
@@ -83,23 +89,23 @@
 
                     ;; Delay in milliseconds between each registration
                     ;; of the SIP-client designed to not overwhelm the
-                    ;; registrar
+                    ;; registrar.
                     :register-delay-ms 100
 
-                    ;; Timeout in milliseconds for a running scenario
+                    ;; Timeout in milliseconds for a running scenario.
                     :thread-timeout-ms 120000
 
-                    ;; Count of clients to use for the performance test
+                    ;; Count of clients to use for the performance test.
                     :clients 400
 
-                    ;; Maximum number of parallel running scenarios
+                    ;; Maximum number of parallel running scenarios.
                     :max-threads 50
 
-                    ;; Duration of the performance test
+                    ;; Duration of the performance test.
                     :duration-m 5
 
-                    ;; Minimum delay in seconds after every scenario
+                    ;; Minimum delay in seconds after every scenario.
                     :scenario-min-delay-s 4
 
-                    ;; Maximum delay in seconds after every scenario
+                    ;; Maximum delay in seconds after every scenario.
                     :scenario-max-delay-s 10}}
